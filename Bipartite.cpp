9f23dccbbb43559ce0ad68ad6d4ac0f97dec8191
JPMM <73991168+docinhoss@users.noreply.github.com>
2021-08-29 11:44:57 +0100

    Update Bipartite.cpp

--- Bipartite.cpp
+++ Bipartite.cpp
@@ -76,21 +76,10 @@ vector<string> a;
 
 //(ll *)m
 
-
+//---------------------------------------------ALGO--------------------------------------------------
 
 void print_matrix(int *m, int r, int c){ for(ll i = 0; i < r; i++) for(ll j = 0; j < c; j++) cout << *((m+i*c)+j) << " \n"[j == c-1];}
-
-vector<int> sieve(int n) {
-	int*arr = new int[n + 1](); 
-	vector<int> vect; 
-	for (int i = 2; i <= n; i++)
-		if (arr[i] == 0) {
-			vect.push_back(i); 
-			for (ll j = (ll(i) * ll(i)); j <= n; j += i)
-			arr[j] = 1;
-		} 
-	return vect;
-}
+vector<int> sieve(int n) { int*arr = new int[n + 1](); vector<int> vect; for (int i = 2; i <= n; i++) if (arr[i] == 0) { vect.push_back(i);  for (ll j = (ll(i) * ll(i)); j <= n; j += i) arr[j] = 1; } return vect;}
 
 //---------------------------------------------PROBLEM------------------------------------------------
 
